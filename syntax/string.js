960a2b392aa5f9007e87876226e552b13a5c19be
mucollabo <mucollabo@gmail.com>
2022-08-09 00:10:49 +0900

    complete web207

---
+++ syntax/string.js
@@ -0,0 +1,3 @@
+console.log(1 + 1);
+console.log('1' + '1');
+console.log('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error esse exercitationem hic illo natus omnis placeat quod. Accusantium alias cupiditate dolorem ea minus sapiente vitae. Eligendi ex iusto omnis similique.'.length);
