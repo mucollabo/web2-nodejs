9c8ec081c6bda7f0fc16a9bd85c9b69dd3cc7f3b
mucollabo <mucollabo@gmail.com>
2022-08-10 00:12:14 +0900

    complete web211

---
+++ syntax/template.js
@@ -0,0 +1,5 @@
+var name = 'charles';
+var letter = `Dear ${name}
+
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error esse exercitationem hic illo natus omnis placeat quod. Accusantium alias cupiditate dolorem ea minus sapiente vitae. Eligendi ex iusto omnis similique.' ${name}`;
+console.log(letter);
---
+++ syntax/variable2.js
@@ -0,0 +1,2 @@
+let letter = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error esse exercitationem hic illo natus omnis placeat quod. Accusantium alias cupiditate dolorem ea minus sapiente vitae. Eligendi ex iusto omnis similique.';
+console.log(letter);
