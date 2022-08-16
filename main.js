8dbfebc9e9125f94d910b9219585b3a9a1ee87ba
mucollabo <mucollabo@gmail.com>
2022-08-16 15:25:53 +0900

    complete web237

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ const app = http.createServer(function (request, response) {
           let description = 'Hello, Node.js';
           let list = templateList(filelist);
           const template = templateHTML(title, list, `
-            <form action="http://localhost:3000/create_process" method="post">
+            <form action="/create_process" method="post">
               <p><input type="text" name="title" placeholder="title"></p>
               <p>
                   <textarea name="description" placeholder="description"></textarea>
@@ -97,9 +97,48 @@ const app = http.createServer(function (request, response) {
               response.end();      
             })
         });
+    } else if(pathname === '/update') {
+        fs.readdir('./data', function(error, filelist) {
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+            const title = queryData.id;
+            let list = templateList(filelist);
+            const template = templateHTML(title, list, 
+              `<form action="/update_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                  <p>
+                      <textarea name="description" placeholder="description">${description}</textarea>
+                  </p>
+                  <p>
+                      <input type="submit">
+                  </p>
+              </form>`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              );
+            response.writeHead(200);
+            response.end(template);
+        });
+      });
+    } else if(pathname === '/update_process') {
+      let body = '';
+      request.on('data', function(data) {
+          body += data;
+      });
+      request.on('end', function() {
+          let post = qs.parse(body);
+          let id = post.id;
+          let title = post.title;
+          let description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error) {
+              fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+                  response.writeHead(302, {Location: `/?id=${title}`});
+                  response.end();      
+              });
+          });
+      });
     } else {
-        response.writeHead(404);
-        response.end('Not found');
-    }
+          response.writeHead(404);
+          response.end('Not found');
+      }
 });
 app.listen(3000);
